59d5618b8b0f7752206df5d7eeab0912206f0aff
mehmetyt <146078819+mehmetyt@users.noreply.github.com>
2024-01-28 12:38:59 +0300

    Delete ActionFilterArea directory

--- ActionFilterArea/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ActionFilterArea.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
